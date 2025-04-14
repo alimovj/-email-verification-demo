63216c5729b0b399476515026705b035aa2f91f7
alimovj <youremail@example.com>
2025-04-14 18:39:31 +0500

    Dispatch email verification job after user registration

--- app/Jobs/SendVerificationEmail.php
+++ app/Jobs/SendVerificationEmail.php
@@ -1,27 +1,24 @@
 <?php
-
-namespace App\Jobs;
-
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Auth\Notifications\VerifyEmail;
 
 class SendVerificationEmail implements ShouldQueue
 {
-    use Queueable;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * Create a new job instance.
-     */
-    public function __construct()
+    public $user;
+
+    public function __construct($user)
     {
-        //
+        $this->user = $user;
     }
 
-    /**
-     * Execute the job.
-     */
-    public function handle(): void
+    public function handle()
     {
-        //
+        $this->user->notify(new VerifyEmail);
     }
 }
